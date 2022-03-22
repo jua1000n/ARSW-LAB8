cf2f5ca033519f4925e734195093d5325e620b3d
Juan Sebastian Cadavid <juan.cadavid-p@mail.escuelaing.edu.co>
2022-03-22 11:43:34 -0500

    PARTE 2

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -27,7 +27,6 @@ var app = (function () {
         };
     };
 
-
     var connectAndSubscribe = function () {
         console.info('Connecting to WS...');
         var socket = new SockJS('/stompendpoint');
@@ -39,12 +38,11 @@ var app = (function () {
             stompClient.subscribe('/topic/newpoint', function (eventbody) {
                 alert(eventbody);
                 var theObject = JSON.parse(eventbody.body)
+                addPointToCanvas(theObject);
             });
         });
 
     };
-    
-
 
     return {
 
